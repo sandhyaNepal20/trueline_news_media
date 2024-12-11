5c390bdbe79bff092bb3050aec3672ff627f1a9a
sandhyaNepal20 <nsandhya192@gmail.com>
2024-12-11 21:05:23 +0545

    First commit

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+// import 'package:trueline_news_media/view/login_view.dart';
+import 'package:flutter/material.dart';
+import 'package:trueline_news_media/view/welcome_view.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WelcomeView(),
+    );
+  }
+}

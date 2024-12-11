9dee250c3b92017e2c7402e1f04d22ada790bc66
sandhyaNepal20 <nsandhya192@gmail.com>
2024-12-11 21:16:06 +0545

    second commit

--- lib/view/welcome_view.dart
+++ lib/view/welcome_view.dart
@@ -7,7 +7,21 @@ class WelcomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Welcome'),
+        elevation: 0, // Remove shadow
+        backgroundColor: Colors.white, // White background for AppBar
+      ),
+      body: Column(
+        mainAxisAlignment:
+            MainAxisAlignment.center, // Center content vertically
+        children: [
+          // Logo
+          const Image(
+            image: AssetImage(
+                'lib/assets/images/logo1.png'), // Replace with your logo path
+            height: 350, // Adjust the height of the logo
+          ),
+          const SizedBox(height: 20), // Spacing between logo and text
+        ],
       ),
     );
   }

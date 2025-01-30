cedcb6847f64a823374503840c7726515233009a
sandhyaNepal20 <nsandhya192@gmail.com>
2025-01-30 15:56:26 +0545

    api model completed

---
+++ lib/features/auth/data/data_source/remote_data_source/auth_remote_datasource.dart
@@ -0,0 +1,100 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:trueline_news_media/app/constants/api_endpoints.dart';
+import 'package:trueline_news_media/features/auth/data/data_source/auth_data_source.dart';
+import 'package:trueline_news_media/features/auth/domain/entity/auth_entity.dart';
+
+class AuthRemoteDataSource implements IAuthDataSource {
+  final Dio _dio;
+
+  AuthRemoteDataSource(this._dio);
+
+  @override
+  Future<void> registerStudent(AuthEntity student) async {
+    try {
+      Response response = await _dio.post(
+        ApiEndpoints.register,
+        data: {
+          "fullName": student.fullName,
+          "email": student.email,
+          "image": student.image,
+          "password": student.password,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        return;
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } on DioException catch (e) {
+      throw Exception(e);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  @override
+  Future<AuthEntity> getCurrentUser() {
+    // TODO: implement getCurrentUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> loginStudent(String email, String password) async {
+    try {
+      Response response = await _dio.post(
+        ApiEndpoints.login,
+        data: {
+          "email": email,
+          "password": password,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final str = response.data['token'];
+        return str;
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } on DioException catch (e) {
+      throw Exception(e);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  @override
+  Future<String> uploadProfilePicture(File file) async {
+    try {
+      String fileName = file.path.split('/').last;
+      FormData formData = FormData.fromMap(
+        {
+          'profilePicture': await MultipartFile.fromFile(
+            file.path,
+            filename: fileName,
+          ),
+        },
+      );
+
+      Response response = await _dio.post(
+        ApiEndpoints.uploadImage,
+        data: formData,
+      );
+
+      if (response.statusCode == 200) {
+        // Extract the image name from the response
+        final str = response.data['data'];
+
+        return str;
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } on DioException catch (e) {
+      throw Exception(e);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+}
--- lib/features/auth/domain/entity/auth_entity.dart
+++ lib/features/auth/domain/entity/auth_entity.dart
@@ -4,15 +4,18 @@ class AuthEntity extends Equatable {
   final String? userId;
   final String fullName;
   final String email;
+  final String? image;
+
   final String password;
 
   const AuthEntity({
     this.userId,
     required this.fullName,
     required this.email,
+    this.image,
     required this.password,
   });
 
   @override
-  List<Object?> get props => [userId, email, password];
+  List<Object?> get props => [userId, email, image, password];
 }

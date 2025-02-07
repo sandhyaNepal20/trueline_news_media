0e9d8fd40394f8323d0600eded13ad0871e8a912
sandhyaNepal20 <nsandhya192@gmail.com>
2025-02-07 12:48:56 +0545

    token mock

---
+++ test/features/auth/domain/use_case/token.mock.dart
@@ -0,0 +1,4 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:trueline_news_media/app/shared_prefs/token_shared_prefs.dart';
+
+class MockTokenSharedPrefs extends Mock implements TokenSharedPrefs {}
